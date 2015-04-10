46df00dfba12402e7ce9be99fe8bf022af0bf545
unknown <faruk.demir@MSKK-YZL-LT9189.bilgeadam.com>
2015-04-10 16:22:35 +0300

    Functionality and Grunt added

--- server/app.js
+++ server/app.js
@@ -1,20 +1,27 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var path=require('path');
+var favicon=require('serve-favicon');
 var app = express();
+
+app.set('view engine','vash');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(favicon(path.normalize(__dirname+'/..')+'/public/favicon.ico'));
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-app.set(express.static(__dirname+'/public'));
-app.set('views',__dirname+"server/views");
+app.use(express.static(path.normalize(__dirname+'/..')+'/public'));
 
-app.get('*',function(req,resp) {
-	console.log("The requested url is "+req.url);
-	resp.end();
+app.set('views', path.normalize(__dirname+'/..') + '/server/views');
+//app.set('views',__dirname+"views");
+
+app.get('/',function(req,resp) {
+	console.log("Request handled for url: "+req.url);
+	resp.render('index');
 });
 
 var server=http.createServer(app);
-server.listen(3000);
+server.listen(4000);
+console.log("Server started to listen 4000 port. Changes ");
